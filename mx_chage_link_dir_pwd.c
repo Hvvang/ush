e4440b386d6936e0d10509c553170791005e2526
“fanbear” <fanbearytb@gmail.com>
2020-03-08 23:23:27 +0200

    cd -

--- mx_chage_link_dir_pwd.c
+++
@@ -1,9 +0,0 @@
-#include "ush.h"
-
-void mx_chage_link_dir_pwd(char *str) {
-    // char *s = NULL;
-    chdir(str);
-    setenv("PWD", str, 1);
-    // mx_strdel(&s);
-    mx_ush_loop();
-}
--- mx_get_array_size.c
+++
@@ -1,8 +0,0 @@
-#include "ush.h"
-
-int mx_get_array_size(char **arr) {
-	int count = 0;
-	for (int i = 0; arr[i]; i++)
-		count++;
-	return count;
-}
